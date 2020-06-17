9027d0b0335dde472350d0c679042dbd4dbad3a0
Edgar <edgrr.trem000@gmail.com>
2020-06-17 23:06:04 +0700

    export data manager

--- lib/base_core.dart
+++ lib/base_core.dart
@@ -2,6 +2,7 @@ library base_core;
 
 export 'src/activity_indicator.dart';
 export 'src/base_bloc.dart';
+export 'src/data_manager.dart';
 export 'src/failure.dart';
 export 'src/usecase.dart';
 export 'src/value_stream_builder.dart';
