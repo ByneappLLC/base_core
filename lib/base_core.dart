05da5a7030418a67cc1c08c1189abd80afb8df7c
Edgar <edgrr.trem000@gmail.com>
2020-07-15 16:21:07 +0700

    export logger

--- lib/base_core.dart
+++ lib/base_core.dart
@@ -4,5 +4,6 @@ export 'src/activity_indicator.dart';
 export 'src/base_bloc.dart';
 export 'src/data_manager.dart';
 export 'src/failure.dart';
+export 'src/register_logger.dart';
 export 'src/usecase.dart';
 export 'src/value_stream_builder.dart';
