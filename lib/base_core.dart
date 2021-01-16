30c62e8fb53b43ba15fdb073f38e769c14ed40bb
Edgar <edgrr.trem000@gmail.com>
2021-01-16 23:40:20 +0700

    export

--- lib/base_core.dart
+++ lib/base_core.dart
@@ -5,5 +5,6 @@ export 'src/base_bloc.dart';
 export 'src/data_manager.dart';
 export 'src/failure.dart';
 export 'src/register_logger.dart';
+export 'src/use_case_generator.dart';
 export 'src/usecase.dart';
 export 'src/value_stream_builder.dart';
